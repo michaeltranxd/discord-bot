f4b8164ba4ba76f0344f9e88f625e9ae5614cf2b
michaeltranxd <mtt2@uic.edu>
2020-08-03 21:44:03 -0500

    Dynamic commands + command handling

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
-const config = require("./config.json");
-
+const fs = require("fs");
 const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 client.once("ready", () => {
   console.log("Ready!");
 });
 
-client.login(config.token);
-
 client.on("message", (message) => {
-  // Ignore the message if it was sent by the bot
-  if (message.author.bot) {
-    return;
-  }
+  // Ignore the message if it was sent by the bot or if it doesn't have our prefix
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  console.log(message.content);
-  if (message.content === "!ping") {
-    // send back "Pong." to the channel the message was sent in
-    message.channel.send("Pong.");
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
+
+  // command is not valid (we dont recognize this command)
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName);
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
   }
 });
+
+client.login(token);
---
+++ src/templates/command_template.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "insert name",
+  description: "insert description",
+  execute(message, args) {
+    console.log("how'd they know");
+  },
+};
